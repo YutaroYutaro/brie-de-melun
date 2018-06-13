afd79a57eda6b37d5e6ae7cf10500af6e268afd5
yutaro <b3122047@gmail.com>
2018-06-13 17:09:31 +0900

    close #1 マップをランダム生成できるように変更．草原，森，山，金鉱のオブジェクトを仮作成．

---
+++ Assets/Scenes/Scripts/CreateMap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMap : MonoBehaviour {
+
+    //生成するマップオブジェクト
+    public GameObject[] mapObject;
+
+    //生成するマップの大きさ
+    public int maxPosX = 5;
+    public int maxPosZ = 7;
+
+    //マップオブジェクトを生成する座標
+    private int posX = 0;
+    private int posZ = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        //
+        for (; posX < maxPosX; posX++)
+        {
+            for (; posZ < maxPosZ; posZ++)
+            {
+                int objectNumber = Random.Range(0, mapObject.Length);
+
+                Vector3 position = new Vector3(posX, 0, posZ);
+                Quaternion q = new Quaternion();
+                q = Quaternion.identity;
+
+                Instantiate(mapObject[objectNumber], position, q);
+            }
+
+            posZ = 0;
+        }
+    }
+}
