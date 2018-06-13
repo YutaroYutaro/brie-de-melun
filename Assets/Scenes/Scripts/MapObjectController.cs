968ac80d0dbadb33ecee4c3f0c14577028a74a6b
yutaro <b3122047@gmail.com>
2018-06-13 23:30:58 +0900

    #2 マウスオーバーのフィードバックとクリックした地点への移動

---
+++ Assets/Scenes/Scripts/MapObjectController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class MapObjectController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    //Material thisMaterial;
+    Color defaultColor;
+    //Renderer rend;
+
+    void Start()
+    {
+        //thisMaterial = GetComponent<Renderer>().material;
+        defaultColor = GetComponent<Renderer>().material.color;
+
+        Debug.Log(defaultColor);
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+
+        //rend = GetComponent<Renderer>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponent<Renderer>().material.color = defaultColor;
+    }
+}
---
+++ Assets/Scenes/Scripts/UnitMove.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMove : MonoBehaviour
+{
+    //NavMeshAgent agent;
+
+    float distance = 0.0f;
+    CharacterController controller;
+
+    void Start()
+    {
+        //メインカメラとオブジェクトの距離を測定
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        Debug.Log("distance ->" + distance);
+
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //クリックしたスクリーン座標を取得
+            Vector3 mousePos = Input.mousePosition;
+            Debug.Log(mousePos);
+
+            //メインカメラとオブジェクトの距離をクリックした座標の高さに代入
+            mousePos.z = distance;
+            Debug.Log(mousePos);
+
+            //ワールド座標に変換
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(mousePos);
+
+            //座標の四捨五入
+            worldPoint.x = Mathf.RoundToInt(worldPoint.x);
+            worldPoint.z = Mathf.RoundToInt(worldPoint.z);
+
+            //オブジェクトの移動
+            transform.position = worldPoint;
+            //controller.SimpleMove(worldPoint);
+            Debug.Log(worldPoint);
+
+
+        }
+    }
+}
