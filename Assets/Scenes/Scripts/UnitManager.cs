ffb8490c9525d352a0c31217d756613aa805c650
yutaro <b3122047@gmail.com>
2018-07-01 16:20:10 +0900

    #11 UnitManagerの追加．生成された操作ユニットの管理を行う．

---
+++ Assets/Scenes/Scripts/UnitManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitManager : MonoBehaviour
+{
+	public List<GameObject> UnitList;
+
+	public GameObject CreatedUnit;
+	
+	// Use this for initialization
+	void Start () {
+		UnitList = new List<GameObject>();
+		
+		GameObject toggleInstance = Instantiate(CreatedUnit) as GameObject;
+		
+		UnitList.Add(toggleInstance);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
