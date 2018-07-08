70bed04197903389fc6366eec3893647cef45317
yutaro <b3122047@gmail.com>
2018-07-08 23:29:17 +0900

    #14 カードのプレハブ化．ハンドの追加．ドラッグしたらハンドに戻るように設定．カメラの解像度を変更．

--- Assets/Scenes/Scripts/Draggable.cs
+++ Assets/Scenes/Scripts/Draggable.cs
@@ -8,34 +8,40 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
 	
 	RectTransform rectTransform = null;
 	
+	private Transform parentToReturnTo = null;
 	
 	void Awake()
 	{
 		rectTransform = GetComponent<RectTransform> ();
 	}
+	
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent(this.transform.parent.parent);
+		Debug.Log("OnBeginDrag");
+	}
 
 	public void OnDrag(PointerEventData eventData)
 	{
 		Debug.Log(eventData);
-		
+//		this.transform.position = eventData.position;
+
 		Vector3 result = Vector3.zero;
 
 		if (RectTransformUtility.ScreenPointToWorldPointInRectangle(rectTransform, eventData.position, Camera.main,
 			out result))
 		{
 			rectTransform.position = result;
+			Debug.Log("OnDrag");
 		}
-		
-	}
 
-	public void OnBeginDrag(PointerEventData eventData)
-	{
-		Debug.Log("OnBiginDrag");
 	}
 
 	public void OnEndDrag(PointerEventData eventData)
 	{
 		Debug.Log("OnEndDtag");
-	} 
+		this.transform.SetParent(parentToReturnTo);
+	}
 	
 }
