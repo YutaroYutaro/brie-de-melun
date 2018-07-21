770a480d54bd8b2ba0ec657c89b675fb77be7eda
yutaro <b3122047@gmail.com>
2018-07-21 22:15:55 +0900

    #20 カードフローマネージャーを追加．

---
+++ Assets/Scenes/Scripts/CardFlowManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardFlowManager : MonoBehaviour
+{
+	private GameObject _deckText;
+	private GameObject _graveyardText;
+
+	public List<GameObject> DeckCardList;
+	public List<GameObject> GraveyardCardList;
+
+	// Use this for initialization
+	void Start () {
+		_deckText = GameObject.Find("Deck");
+		_graveyardText = GameObject.Find("Graveyard");
+		
+	}
+}
