c5f72c58cbb72b80bc4bde63a3c496ba29a8e200
yutaro <b3122047@gmail.com>
2018-08-12 16:46:26 +0900

    #24 偵察ユニットの追加．

---
+++ Assets/Scenes/Scripts/UnitReconnaissanceController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitReconnaissanceController : MonoBehaviour {
+
+    public void UnitReconnaissance()
+    {
+        int thisIntPosX = GetComponent<UnitOwnIntPosition>().PosX;
+        int thisIntPosZ = GetComponent<UnitOwnIntPosition>().PosZ;
+
+        GameObject.Find("FogManager").GetComponent<FogManager>().ClearFog(thisIntPosX + 1, thisIntPosZ);
+        GameObject.Find("FogManager").GetComponent<FogManager>().ClearFog(thisIntPosX - 1, thisIntPosZ);
+        GameObject.Find("FogManager").GetComponent<FogManager>().ClearFog(thisIntPosX, thisIntPosZ + 1);
+        GameObject.Find("FogManager").GetComponent<FogManager>().ClearFog(thisIntPosX, thisIntPosZ - 1);
+    }
+}
--- Assets/Scenes/Scripts/UnitSummonGenerator.cs
+++ Assets/Scenes/Scripts/UnitSummonGenerator.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class UnitSummonGenerator : MonoBehaviour
 {
     public GameObject ProximityAttackPrefab;
     public GameObject RemoteAttackPrefab;
-    public GameObject SummonReconnaissanecPrefab;
+    public GameObject ReconnaissanecPrefab;
+
+    public int _summonUnitType;
 
     public void SummonProximityAttackUnit(int posX, int posZ)
     {
@@ -27,10 +30,21 @@ public class UnitSummonGenerator : MonoBehaviour
         proximityUnit.transform.SetParent(GameObject.Find("Player1Units").transform);
     }
 
-    public void SummonReconnaissanceUnit()
+    public void SummonReconnaissanceUnit(int posX, int posZ)
     {
-        GameObject proximityUnit = Instantiate(SummonReconnaissanecPrefab);
+        GameObject reconnaissanecUnit = Instantiate(ReconnaissanecPrefab);
 
-        proximityUnit.transform.SetParent(GameObject.Find("Player1Units").transform);
+        reconnaissanecUnit.transform.SetParent(GameObject.Find("Player1Units").transform);
+
+        GameObject.Find("UnitManager").GetComponent<UnitManager>().GetMyUnitList().Add(reconnaissanecUnit);
+
+        reconnaissanecUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
+        reconnaissanecUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
+    }
+
+    public int SummonUnitType
+    {
+        get { return _summonUnitType; }
+        set { _summonUnitType = value; }
     }
 }
