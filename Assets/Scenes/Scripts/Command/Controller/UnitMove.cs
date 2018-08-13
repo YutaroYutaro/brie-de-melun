b9837fff7e5092ed0e58537b4acbbd9828a1f975
yutaro <b3122047@gmail.com>
2018-08-13 20:47:53 +0900

    #34 MoveUnitクラスのリファクタリング

--- Assets/Scenes/Scripts/Command/Controller/UnitMove.cs
+++ Assets/Scenes/Scripts/Command/Controller/UnitMove.cs
@@ -6,15 +6,90 @@ using UnityEngine;
 
 public class UnitMove : MonoBehaviour
 {
-    Nodes[,] resultNodes;
-    ShortestPath ShortestPath;
-
-    Vector3 worldPoint;
-    Vector3 _nextDestination;
-
-    void Start()
+    public async void MiniMapClickUnitMove(
+        int clickMiniMapImageInstancePositionX,
+        int clickMiniMapImageInstancePositionZ
+    )
     {
-        ShortestPath = new ShortestPath();
+        ShortestPath shortestPath = new ShortestPath();
+
+        //ダイクストラ法で最短経路を検索
+        Nodes[,] resultNodes =
+            shortestPath.DijkstraAlgorithm(
+                clickMiniMapImageInstancePositionX,
+                clickMiniMapImageInstancePositionZ
+                );
+
+        //現在地を目的地として設定
+        //ノードが後ろから繋がっているため
+        Nodes unitPositionNode =
+            resultNodes[
+                GetComponent<UnitOwnIntPosition>().PosX,
+                GetComponent<UnitOwnIntPosition>().PosZ
+            ];
+
+        Debug.Log("========================================");
+
+        string path = "Start -> ";
+        Nodes currentNode = unitPositionNode;
+
+        while (true)
+        {
+            //ひとつ前のノードを参照
+            Nodes nextNode = currentNode.PreviousNodes;
+
+            //クリックしたノードに達するとループを抜ける
+            if (nextNode == null || ExistUnit(nextNode.IdX, nextNode.IdZ))
+            {
+                path += " Goal";
+                break;
+            }
+
+            float unitTypePosY;
+
+            switch (tag)
+            {
+                case "ProximityAttackUnit":
+                case "RemoteAttackUnit":
+                    unitTypePosY = 1;
+                    break;
+                case "ReconnaissanceUnit":
+                    unitTypePosY = 1.5f;
+                    break;
+                default:
+                    unitTypePosY = 1;
+                    break;
+            }
+
+            Vector3 nextDestination = new Vector3(nextNode.IdX, unitTypePosY, nextNode.IdZ);
+
+            GameObject.Find("FogManager")
+                .GetComponent<FogManager>()
+                .ClearFog(
+                    nextNode.IdX,
+                    nextNode.IdZ
+                );
+
+            GetComponent<UnitOwnIntPosition>()
+                .SetUnitOwnIntPosition(
+                    nextNode.IdX,
+                    nextNode.IdZ
+                );
+
+            //次に移動するノードに移動
+            transform.DOMove(nextDestination, 0.4f);
+
+            //待機
+            await Task.Delay(TimeSpan.FromSeconds(0.5f));
+
+            path += nextNode.IdX.ToString() + nextNode.IdZ.ToString() + " -> ";
+
+            //次のノードへ
+            currentNode = nextNode;
+        }
+
+        Debug.Log(path);
+        Debug.Log("========================================");
     }
 
     private bool ExistUnit(int posX, int posZ)
@@ -58,73 +133,5 @@ public class UnitMove : MonoBehaviour
         }
 
         return false;
-    }
-
-    public async void MiniMapClickUnitMove(
-        int clickMiniMapImageInstancePositionX,
-        int clickMiniMapImageInstancePositionZ
-    )
-    {
-        //ダイクストラ法で最短経路を検索
-        resultNodes =
-            ShortestPath.DijkstraAlgorithm(clickMiniMapImageInstancePositionX, clickMiniMapImageInstancePositionZ);
-
-        //現在地を目的地として設定
-        //ノードが後ろから繋がっているため
-        Nodes unitPositionNode =
-            resultNodes[Mathf.RoundToInt(transform.position.x), Mathf.RoundToInt(transform.position.z)];
-
-        Debug.Log("========================================");
-
-        string path = "Start -> ";
-        Nodes currentNode = unitPositionNode;
-
-        while (true)
-        {
-            //ひとつ前のノードを参照
-            Nodes nextNode = currentNode.PreviousNodes;
-
-            //クリックしたノードに達するとループを抜ける
-            if (nextNode == null || this.ExistUnit(nextNode.IdX, nextNode.IdZ))
-            {
-                path += " Goal";
-                break;
-            }
-
-            //次に移動するノードの座標
-            _nextDestination.x = nextNode.IdX;
-            switch (tag)
-            {
-                case "ProximityAttackUnit":
-                case "RemoteAttackUnit":
-                    _nextDestination.y = 1;
-                    break;
-                case "ReconnaissanceUnit":
-                    _nextDestination.y = 1.5f;
-                    break;
-                default:
-                    _nextDestination.y = 1;
-                    break;
-            }
-
-            _nextDestination.z = nextNode.IdZ;
-
-            GameObject.Find("FogManager").GetComponent<FogManager>().ClearFog(nextNode.IdX, nextNode.IdZ);
-            GetComponent<UnitOwnIntPosition>().SetUnitOwnIntPosition(nextNode.IdX, nextNode.IdZ);
-
-            //次に移動するノードに移動
-            transform.DOMove(_nextDestination, 0.4f);
-
-            //待機
-            await Task.Delay(TimeSpan.FromSeconds(0.5f));
-
-            path += nextNode.IdX.ToString() + nextNode.IdZ.ToString() + " -> ";
-
-            //次のノードへ
-            currentNode = nextNode;
-        }
-
-        Debug.Log(path);
-        Debug.Log("========================================");
     }
 }
