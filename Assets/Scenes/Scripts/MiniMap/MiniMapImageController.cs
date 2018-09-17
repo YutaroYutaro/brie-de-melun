82d9313ec2215cc15c03e5741e57e501e53869fa
yutaro <b3122047@gmail.com>
2018-09-17 15:23:03 +0900

    #55 SelectAttackerUnitPhaseクラスのadd.

--- Assets/Scenes/Scripts/MiniMap/MiniMapImageController.cs
+++ Assets/Scenes/Scripts/MiniMap/MiniMapImageController.cs
@@ -29,50 +29,13 @@ public class MiniMapImageController : MonoBehaviour, IPointerClickHandler, IPoin
                 break;
 
             case "SelectAttackerUnit":
-                UnitAttackManager unitAttackManager =
-                    GameObject.Find("UnitAttackManager").GetComponent<UnitAttackManager>();
-
-                List<UnitAttackManager.AttackerAndTarget> attackerAndTargetList =
-                    unitAttackManager.GetAttackerAndTargetList();
-
-                foreach (UnitAttackManager.AttackerAndTarget attackerAndTarget in attackerAndTargetList)
-                {
-                    if (attackerAndTarget.Attacker.GetComponent<UnitOwnIntPosition>().PosX == miniMapPosX &&
-                        attackerAndTarget.Attacker.GetComponent<UnitOwnIntPosition>().PosZ == miniMapPosZ)
-                    {
-                        Debug.Log("Debug: Set Attacker Unit");
-
-                        if (attackerAndTarget.Target.Count == 1)
-                        {
-                            unitAttackManager.SelectedAttacker =
-                                unitAttackManager.GetAttackerAndTargetList().First().Attacker;
-
-                            unitAttackManager.MiniMapUnitAttack(attackerAndTarget.Target[0]);
-
-                            GameObject.Find("PhaseManager")
-                                .GetComponent<PhaseManager>()
-                                .SetNextPhase("SelectUseCard");
-
-                            Debug.Log("Phase: SelectUseCard");
-                        }
-                        else
-                        {
-                            unitAttackManager.SetSelectedAttackerAndTargetUnit(attackerAndTarget);
-                            GameObject.Find("PhaseManager")
-                                .GetComponent<PhaseManager>()
-                                .SetNextPhase("SelectAttackTargetUnit");
-
-                            Debug.Log("Phase: SelectAttackTargetUnit");
-                        }
-
-                        break;
-                    }
-                }
+                _phaseType = new SelectAttackerUnitPhase();
+                _phaseType.PhaseController(miniMapPosX, miniMapPosZ);
 
                 break;
 
             case "SelectAttackTargetUnit":
-                unitAttackManager =
+                UnitAttackManager unitAttackManager =
                     GameObject.Find("UnitAttackManager").GetComponent<UnitAttackManager>();
 
                 List<GameObject> selectedTargetList =
