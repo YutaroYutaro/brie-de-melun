75bd12de2aaf8ea24432b63202e8aad64c43ab4e
yutaro <b3122047@gmail.com>
2018-09-25 06:55:01 +0900

    #38 ShopCanvasオブジェクトの追加．ShopCanvasクラスの追加．

---
+++ Assets/Scenes/Scripts/ShopCanvas.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopCanvas : MonoBehaviour {
+
+    void Awake()
+    {
+        GetComponent<Canvas>().enabled = false;
+    }
+}
