b4e753176cf639f4ef77f75f00ddd743e8cce987
yutaro <b3122047@gmail.com>
2018-10-07 19:40:29 +0900

    #38 ショップ内のカードをEventTriggerで判定．

---
+++ Assets/Scenes/Scripts/Card/CardEventTrigerInShopPresenter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx.Triggers;
+using UniRx;
+
+public class CardEventTrigerInShopPresenter : MonoBehaviour
+{
+    void Start()
+    {
+        ObservableEventTrigger observableEventTrigger = gameObject.AddComponent<ObservableEventTrigger>();
+
+        observableEventTrigger.OnPointerDownAsObservable()
+            .Subscribe(eventData =>
+                Debug.Log(eventData.pointerEnter.name)
+            );
+    }
+}
---
+++ Assets/Scenes/Scripts/Command/Presenter/ShopPresenter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace Scenes.Scripts.Command.Presenter
+{
+    public class ShopPresenter : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _button;
+
+        void Start()
+        {
+            _button.OnClickAsObservable()
+                .Subscribe(_ => ShopCanvas.Instance.ShopCanvasClose());
+        }
+    }
+}
