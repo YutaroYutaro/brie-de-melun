658a827a31fff536351b573c8bac49fad6aff7d3
yutaro <b3122047@gmail.com>
2018-10-15 20:10:02 +0900

    #64 Networkクラスを修正．

--- Assets/Scenes/Scripts/Network.cs
+++ Assets/Scenes/Scripts/Network.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class Network : MonoBehaviour
 {
-    void Start()
-    {
+    void Start() {
+        // Photonに接続する(引数でゲームのバージョンを指定できる)
         PhotonNetwork.ConnectUsingSettings(null);
     }
 
-    void OnJoinedLobby()
-    {
-        Debug.Log("OnJoinedLobby()");
+    // ロビーに入ると呼ばれる
+    void OnJoinedLobby() {
+        Debug.Log("ロビーに入りました。");
+
+        // ルームに入室する
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    // ルームに入室すると呼ばれる
+    void OnJoinedRoom() {
+        Debug.Log("ルームへ入室しました。");
+    }
+
+    // ルームの入室に失敗すると呼ばれる
+    void OnPhotonRandomJoinFailed() {
+        Debug.Log("ルームの入室に失敗しました。");
+
+        // ルームがないと入室に失敗するため、その時は自分で作る
+        // 引数でルーム名を指定できる
+        PhotonNetwork.CreateRoom("myRoomName");
     }
 }
