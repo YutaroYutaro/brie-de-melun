2ebe68dbd4c48ffbd7bc4a00804ed240b5d3b4a5
yutaro <b3122047@gmail.com>
2018-10-15 19:34:19 +0900

    #64 RPCメソッドの追加．

--- Assets/Scenes/Scripts/Command/Generator/UnitSummonGenerator.cs
+++ Assets/Scenes/Scripts/Command/Generator/UnitSummonGenerator.cs
@@ -20,6 +20,9 @@ public class UnitSummonGenerator : MonoBehaviour
         proximityUnit.transform.SetParent(GameObject.Find("Player1Units").transform);
         proximityUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
         proximityUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
+
+        PhotonView  photonView  = GetComponent<PhotonView>();
+        photonView.RPC( "SummonEnemyProximityAttackUnit", PhotonTargets.Others, 4 - posX, 6 - posZ);
     }
 
     public void SummonRemoteAttackUnit(int posX, int posZ)
@@ -46,6 +49,51 @@ public class UnitSummonGenerator : MonoBehaviour
             );
 
         reconnaissanecUnit.transform.SetParent(GameObject.Find("Player1Units").transform);
+        reconnaissanecUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
+        reconnaissanecUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
+    }
+
+    [PunRPC]
+    public void SummonEnemyProximityAttackUnit(int posX, int posZ)
+    {
+        GameObject proximityUnit =
+            Instantiate(
+                ProximityAttackPrefab,
+                new Vector3(posX, 0.5f, posZ),
+                Quaternion.identity
+            );
+
+        proximityUnit.transform.SetParent(GameObject.Find("Player2Units").transform);
+        proximityUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
+        proximityUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
+    }
+
+    [PunRPC]
+    public void SummonEnemyRemoteAttackUnit(int posX, int posZ)
+    {
+        GameObject remoteAttackUnit =
+            Instantiate(
+                RemoteAttackPrefab,
+                new Vector3(posX, 0.5f, posZ),
+                Quaternion.identity
+            );
+
+        remoteAttackUnit.transform.SetParent(GameObject.Find("Player2Units").transform);
+        remoteAttackUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
+        remoteAttackUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
+    }
+
+    [PunRPC]
+    public void SummonEnemyReconnaissanceUnit(int posX, int posZ)
+    {
+        GameObject reconnaissanecUnit =
+            Instantiate(
+                ReconnaissanecPrefab,
+                new Vector3(posX, 1.5f, posZ),
+                Quaternion.identity
+            );
+
+        reconnaissanecUnit.transform.SetParent(GameObject.Find("Player2Units").transform);
         reconnaissanecUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
         reconnaissanecUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
     }
---
+++ Assets/Scenes/Scripts/Network.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Network : MonoBehaviour
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(null);
+    }
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("OnJoinedLobby()");
+    }
+}
