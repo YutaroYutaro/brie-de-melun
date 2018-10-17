d6ebf8a3c36279ca42b99554ef38d23755663615
yutaro <b3122047@gmail.com>
2018-10-18 08:04:34 +0900

    #64 召喚，移動のRPCを追加

--- Assets/Scenes/Scripts/Command/Generator/UnitSummonGenerator.cs
+++ Assets/Scenes/Scripts/Command/Generator/UnitSummonGenerator.cs
@@ -21,8 +21,14 @@ public class UnitSummonGenerator : MonoBehaviour
         proximityUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
         proximityUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
 
-        PhotonView  photonView  = GetComponent<PhotonView>();
-        photonView.RPC( "SummonEnemyProximityAttackUnit", PhotonTargets.Others, 4 - posX, 6 - posZ);
+        int id = PhotonNetwork.AllocateViewID();
+        proximityUnit.GetComponent<ViewId>().UnitViewId = id;
+
+        EnemyUnitController.Instance.SummonUnit(
+            new Vector3(4 - posX, 0.5f, 6 - posZ),
+            Quaternion.identity,
+            id
+        );
     }
 
     public void SummonRemoteAttackUnit(int posX, int posZ)
@@ -49,51 +55,6 @@ public class UnitSummonGenerator : MonoBehaviour
             );
 
         reconnaissanecUnit.transform.SetParent(GameObject.Find("Player1Units").transform);
-        reconnaissanecUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
-        reconnaissanecUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
-    }
-
-    [PunRPC]
-    public void SummonEnemyProximityAttackUnit(int posX, int posZ)
-    {
-        GameObject proximityUnit =
-            Instantiate(
-                ProximityAttackPrefab,
-                new Vector3(posX, 0.5f, posZ),
-                Quaternion.identity
-            );
-
-        proximityUnit.transform.SetParent(GameObject.Find("Player2Units").transform);
-        proximityUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
-        proximityUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
-    }
-
-    [PunRPC]
-    public void SummonEnemyRemoteAttackUnit(int posX, int posZ)
-    {
-        GameObject remoteAttackUnit =
-            Instantiate(
-                RemoteAttackPrefab,
-                new Vector3(posX, 0.5f, posZ),
-                Quaternion.identity
-            );
-
-        remoteAttackUnit.transform.SetParent(GameObject.Find("Player2Units").transform);
-        remoteAttackUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
-        remoteAttackUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
-    }
-
-    [PunRPC]
-    public void SummonEnemyReconnaissanceUnit(int posX, int posZ)
-    {
-        GameObject reconnaissanecUnit =
-            Instantiate(
-                ReconnaissanecPrefab,
-                new Vector3(posX, 1.5f, posZ),
-                Quaternion.identity
-            );
-
-        reconnaissanecUnit.transform.SetParent(GameObject.Find("Player2Units").transform);
         reconnaissanecUnit.GetComponent<UnitOwnIntPosition>().PosX = posX;
         reconnaissanecUnit.GetComponent<UnitOwnIntPosition>().PosZ = posZ;
     }
---
+++ Assets/Scenes/Scripts/EnemyUnitController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx.Async;
+using DG.Tweening;
+using System.Threading.Tasks;
+using System;
+
+public class EnemyUnitController : SingletonMonoBehaviour<EnemyUnitController>
+{
+    [SerializeField] private GameObject _proximityAttackPrefab;
+    [SerializeField] private GameObject _remoteAttackPrefab;
+    [SerializeField] private GameObject _reconnaissanecPrefab;
+
+    public void UnitMove(int id, int posX, float posY, int posZ)
+    {
+        PhotonView photonView = GetComponent<PhotonView>();
+        photonView.RPC("EnemyUnitMove", PhotonTargets.All, id, posX, posY, posZ);
+    }
+
+    [PunRPC]
+    public void EnemyUnitMove(int id, int posX, float posY, int posZ)
+    {
+        PhotonView unit = PhotonView.Find(id);
+        Vector3 nextDestination = new Vector3(4 - posX, posY, 6 - posZ);
+
+        unit.transform.DOMove(nextDestination, 1.3f);
+    }
+
+    public void SummonUnit(Vector3 pos, Quaternion rot, int id)
+    {
+        PhotonView photonView = GetComponent<PhotonView>();
+        photonView.RPC(
+            "SummonEnemyProximityAttackUnit",
+            PhotonTargets.All,
+            pos,
+            rot,
+            id
+        );
+    }
+
+    [PunRPC]
+    public void SummonEnemyProximityAttackUnit(Vector3 pos, Quaternion rot, int id)
+    {
+        GameObject newPlayer = Instantiate(_proximityAttackPrefab, pos, rot);
+        newPlayer.transform.eulerAngles = new Vector3 (0, 180f, 0);
+
+        // Set player's PhotonView
+        PhotonView[] nViews = newPlayer.GetComponents<PhotonView>();
+        nViews[0].viewID = id;
+
+        newPlayer.transform.SetParent(GameObject.Find("Player2Units").transform);
+
+        Debug.Log(nViews);
+        Debug.Log(nViews[0].viewID);
+    }
+
+    [PunRPC]
+    public void SummonEnemyRemoteAttackUnit(Vector3 pos, Quaternion rot, int id)
+    {
+        GameObject newPlayer = Instantiate(_remoteAttackPrefab, pos, rot);
+
+        // Set player's PhotonView
+        PhotonView[] nViews = newPlayer.GetComponentsInChildren<PhotonView>();
+        nViews[nViews.Length].viewID = id;
+    }
+
+    [PunRPC]
+    public void SummonEnemyReconnaissanceUnit(Vector3 pos, Quaternion rot, int id)
+    {
+        GameObject newPlayer = Instantiate(_reconnaissanecPrefab, pos, rot);
+
+        // Set player's PhotonView
+        PhotonView[] nViews = newPlayer.GetComponentsInChildren<PhotonView>();
+        nViews[nViews.Length].viewID = id;
+    }
+}
