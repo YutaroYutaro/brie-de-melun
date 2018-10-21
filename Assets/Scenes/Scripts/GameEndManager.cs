61fa975c85f3c8f719d25412148c0566ddc332f7
yutaro <b3122047@gmail.com>
2018-10-21 23:05:48 +0900

    #68 BreakTowerController, TowerModel, TowerPresenter, EndGameManagerクラス

---
+++ Assets/Scenes/Scripts/GameEndManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEndManager : SingletonMonoBehaviour<GameEndManager>
+{
+    public void GameEnd()
+    {
+        Debug.Log("You Win!");
+    }
+}
---
+++ Assets/Scenes/Scripts/TowerPresenter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class TowerPresenter : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        TowerModel.Instance.TowerHitPointReactiveProperty
+            .Where(towerHitPoint =>
+                towerHitPoint == 2 || towerHitPoint == 1
+            )
+            .Subscribe(_ =>
+                Debug.Log("Tower Break!")
+            );
+
+        TowerModel.Instance.TowerHitPointReactiveProperty
+            .Where(towerHitPoint =>
+                towerHitPoint <= 0
+            )
+            .Subscribe(_ =>
+                GameEndManager.Instance.GameEnd()
+            );
+    }
+}
