7c0667d8bd366c1e1e2c6f6516147eeac667d954
yutaro <b3122047@gmail.com>
2018-11-02 19:05:52 +0900

    #70 2人い女王は入れないように変更

--- Assets/Scenes/Scripts/Network.cs
+++ Assets/Scenes/Scripts/Network.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Network : MonoBehaviour
 {
-    void Start() {
+    private RoomOptions _roomOptions;
+
+    void Start()
+    {
         // Photonに接続する(引数でゲームのバージョンを指定できる)
         PhotonNetwork.ConnectUsingSettings(null);
+        _roomOptions = new RoomOptions()
+        {
+            MaxPlayers = 2,
+            IsOpen = true,
+            IsVisible = true
+        };
     }
 
     // ロビーに入ると呼ばれる
-    void OnJoinedLobby() {
+    void OnJoinedLobby()
+    {
         Debug.Log("ロビーに入りました。");
 
         // ルームに入室する
@@ -18,19 +28,22 @@ public class Network : MonoBehaviour
     }
 
     // ルームに入室すると呼ばれる
-    void OnJoinedRoom() {
+    void OnJoinedRoom()
+    {
 //        PhaseManager.Instance.SetNextPhase("EnemyTurn");
         Debug.Log("ルームへ入室しました。");
     }
 
     // ルームの入室に失敗すると呼ばれる
-    void OnPhotonRandomJoinFailed() {
+    void OnPhotonRandomJoinFailed()
+    {
         Debug.Log("ルームの入室に失敗しました。");
 
 //        PhaseManager.Instance.SetNextPhase("EnemyTurn");
         PhaseManager.Instance.SetNextPhase("SelectUseCard");
         // ルームがないと入室に失敗するため、その時は自分で作る
         // 引数でルーム名を指定できる
-        PhotonNetwork.CreateRoom("myRoomName");
+        PhotonNetwork.CreateRoom("roomName", _roomOptions, null);
+        Debug.Log("ルームを作成しました。");
     }
 }
